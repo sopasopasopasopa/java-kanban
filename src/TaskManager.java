f6ad8438eaac8b4ee30dc5b579b7ae9630e70821
sopa <vnkvd571@yandex.ru>
2024-07-17 15:46:17 +0300

    реализовала методы создания и вывод всех подзадач эпика

--- src/TaskManager.java
+++ src/TaskManager.java
@@ -4,10 +4,24 @@ public class TaskManager {
 
     int id;
     HashMap<Integer, Task> tasks = new HashMap<>();
-    HashMap<Integer, Subtask> subtasks = new HashMap<>();
     HashMap<Integer, Epic> epics = new HashMap<>();
 
     public TaskManager() {
         this.id = 1;
     }
+
+    public int generatedId() {
+        return id++;
+    }
+
+    public void createTask(Task task) {
+        int taskId = generatedId();
+        task.setTaskId(taskId);
+        tasks.put(taskId, task);
+    }
+
+    public HashMap<Integer, Subtask> getAllSubtasksOfEpics(int epicId) {
+    }
+
+
 }
