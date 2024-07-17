200c701483a2bcad1bbed0801997b0bddddc2b20
sopa <vnkvd571@yandex.ru>
2024-07-17 15:50:33 +0300

    добавила метод добавления подзадач в список подзадач

--- src/Epic.java
+++ src/Epic.java
@@ -2,11 +2,14 @@ import java.util.HashMap;
 
 public class Epic extends Task{
 
-    private HashMap<Integer, Task> subtasks;
+    HashMap<Integer, Subtask> subtasks;
 
-    public Epic(String nameTask, String descriptionTask, int id, Status status) {
+    public Epic(String nameTask, String descriptionTask, Status status) {
         super(nameTask, descriptionTask, status);
         this.subtasks = new HashMap<>();
     }
 
+    public void addSubtasks(Subtask subtask) {
+        subtasks.put(subtask.getTaskId(), subtask);
+    }
 }
