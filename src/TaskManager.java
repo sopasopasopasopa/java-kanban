5c5d0b9a69deb2bc44384977ce13dee10172ec0d
sopa <vnkvd571@yandex.ru>
2024-07-17 13:24:04 +0300

    создала

--- src/TaskManager.java
+++ src/TaskManager.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class TaskManager {
+import java.util.HashMap;
+
+public class TaskManager {
+
+    int id;
+    HashMap<Integer, Task> tasks = new HashMap<>();
+    HashMap<Integer, Subtask> subtasks = new HashMap<>();
+    HashMap<Integer, Epic> epics = new HashMap<>();
+
+    public
 }
