bfedc1e724a27c606c36baa4370e293131151e3b
sopa <vnkvd571@yandex.ru>
2024-08-22 16:22:28 +0300

    Внести изменения в старые тесты.

--- tests/TaskTest.java
+++ tests/TaskTest.java
@@ -67,6 +67,69 @@ class TaskTest {
         assertArrayEquals(arrayOne, arrayTwo, "задачи (по всем полям) при добавлении задачи в менеджер неизменны");
     }
 
+    @Test
+    void inMemoryTaskManagerAddsDifferentTaskTypes() {
+        TaskManager manager = Managers.getDefault();
+        Task task1 = new Task(1, "task1", "task number one");
+        Epic epic1 = new Epic(2,"epic1", "epic number one");
+        Subtask subtask1 = new Subtask(3, "subtask1", "subtask number one", 2);
+
+        manager.createTask(task1);
+        manager.createEpic(epic1);
+        manager.createSubtask(epic1, subtask1);
+
+        manager.getTaskById(1);
+        manager.getEpicById(2);
+        manager.getSubtaskById(3);
+
+        List<Task> history = manager.getHistory();
+
+        assertEquals(3, history.size());
+        assertEquals(task1, history.getFirst());
+        assertEquals(epic1, history.get(1));
+        assertEquals(subtask1, history.get(2));
+    }
+
+    @Test
+    public void tasksWithSameIdDoNotConflict() {
+        TaskManager manager = Managers.getDefault();
+
+        Task task1 = new Task(1, "task1", "task number one");
+        manager.createTask(task1);
+
+        Task task2 = new Task("task2", "task number two");
+        manager.createTask(task2);
+
+        Task retrievedTask1 = manager.getTaskById(1);
+        Task retrievedTask2 = manager.getTaskById(task2.getTaskId());
+
+        assertNotEquals(retrievedTask1.getTaskId(), retrievedTask2.getTaskId());
+        assertEquals("task1", retrievedTask1.getNameTask());
+        assertEquals("task number one", retrievedTask1.getDescriptionTask());
+
+        assertEquals("task2", retrievedTask2.getNameTask());
+        assertEquals("task number two", retrievedTask2.getDescriptionTask());
+
+    }
+
+    @Test
+    public void historyManagerPreservesPreviousTaskVersions() {
+        TaskManager manager = new InMemoryTaskManager(new InMemoryHistoryManager());
+        Task task1 = new Task(1, "task1", "task number one");
+        manager.createTask(task1);
+        manager.getTaskById(1);
+        List<Task> historyAfterFirstAdd = manager.getHistory();
+
+        Task task2 = new Task(2, "task1 updated", "updated description");
+        manager.createTask(task2);
+        manager.getTaskById(2);
+        List<Task> historyAfterSecondAdd = manager.getHistory();
+
+        assertEquals(task1, historyAfterSecondAdd.get(0), "Первая версия задачи должна быть сохранена в истории");
+        assertEquals(task2, historyAfterSecondAdd.get(1), "Вторая версия задачи должна быть сохранена в истории");
+    }
+
+
     @BeforeEach
     void setup() {
         historyManager = new InMemoryHistoryManager();
@@ -128,58 +191,13 @@ class TaskTest {
     @Test
     void testHistoryLimit() {
         for (int i = 0; i < 15; i++) {
-            Task task1 = new Task(1, "task " + i , "task number " + i);
+            Task task1 = new Task(i, "task " + i , "task number " + i);
             historyManager.addTask(task1);
         }
 
         List<Task> history = historyManager.getHistory();
         assertEquals(10, history.size());
-        assertEquals(new Task(5, "task 5", "task number 5"), history.get(0));
+        assertEquals(new Task(5, "task 5", "task number 5"), history.getFirst());
     }
 
 }
-
-//    @Test
-//    public void inMemoryTaskManagerAddsDifferentTaskTypes() {
-//        TaskManager manager = Managers.getDefault();
-//        Task task1 = new Task(1, "task1", "task number one");
-//        Epic epic1 = new Epic(2,"epic1", "epic number one");
-//        Subtask subtask1 = new Subtask(3, "subtask1", "subtask number one", 2);
-//        manager.createTask(task1);
-//        manager.createEpic(epic1);
-//        manager.createSubtask(epic1, subtask1);
-//        manager.getTaskById(1);
-//        manager.getEpicById(2);
-//        manager.getSubtaskById(3);
-//        assertEquals(manager.getHistory().size(), 3);
-//    }
-
-//    @Test
-//    public void tasksWithSameIdDoNotConflict() {
-//        TaskManager manager = Managers.getDefault();
-//        Task task1 = new Task(1, "task1", "task number one");
-//        manager.createTask(task1);
-//        Task[] arrayOne = new Task[]{task1};
-//        Task task2 = new Task("task2", "task number two");
-//        Task[] arrayTwo = new Task[]{task2};
-//        assertArrayEquals(arrayOne, arrayTwo, "задачи с заданным id и сгенерированным id не конфликтуют внутри менеджера");
-//    }
-
-//    @Test
-//    public void historyManagerPreservesPreviousTaskVersions() {
-//        TaskManager manager = new InMemoryTaskManager(new InMemoryHistoryManager());
-//        Task task1 = new Task(1, "task1", "task number one");
-//        manager.createTask(task1);
-//        manager.getTaskById(1);
-//        List<Task> historyAfterFirstAdd = manager.getHistory();
-//
-//        Task task2 = new Task(2, "task1 updated", "updated description");
-//        manager.createTask(task2);
-//        manager.getTaskById(2);
-//        List<Task> historyAfterSecondAdd = manager.getHistory();
-//
-//        assertEquals(task1, historyAfterSecondAdd.get(0), "Первая версия задачи должна быть сохранена в истории");
-//        assertEquals(task2, historyAfterSecondAdd.get(1), "Вторая версия задачи должна быть сохранена в истории");
-//    }
-//
-//}
