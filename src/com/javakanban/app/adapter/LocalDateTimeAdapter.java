c9d0acb5e7d86bf6d1eb262a04e2016fe3976812
sopa <vnkvd571@yandex.ru>
2024-11-25 15:48:59 +0300

    создать LocalDateTimeAdapter, DurationAdapter, чтобы контролировать процесс сериализации и десериализации

---
+++ src/com/javakanban/app/adapter/LocalDateTimeAdapter.java
@@ -0,0 +1,32 @@
+package com.javakanban.app.adapter;
+
+import com.google.gson.TypeAdapter;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonWriter;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class LocalDateTimeAdapter extends TypeAdapter<LocalDateTime> {
+
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ISO_LOCAL_DATE_TIME;
+
+    @Override
+    public void write(JsonWriter out, LocalDateTime value) throws IOException {
+        if (value == null) {
+            out.nullValue();
+        } else {
+            out.value(value.format(formatter));
+        }
+    }
+
+    @Override
+    public LocalDateTime read(JsonReader in) throws IOException {
+        if (in != null) {
+            String value = in.nextString();
+            return LocalDateTime.parse(value, formatter);
+        }
+        return null;
+    }
+}
