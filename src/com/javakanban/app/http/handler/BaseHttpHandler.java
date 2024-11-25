3f49ffdc6fbfbdb38d1ed106ba8a62344b361ac6
sopa <vnkvd571@yandex.ru>
2024-11-25 15:46:00 +0300

    создать абстрактный класс BaseHttpHandler с методами sendText,sendNotFound,sendHasInteractions

---
+++ src/com/javakanban/app/http/handler/BaseHttpHandler.java
@@ -0,0 +1,27 @@
+package com.javakanban.app.http.handler;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
+
+public abstract class BaseHttpHandler implements HttpHandler {
+    protected void sendText(HttpExchange exchange, String response, int statusCode) throws IOException {
+        byte[] resp = response.getBytes(StandardCharsets.UTF_8);
+        exchange.getResponseHeaders().add("Content-Type", "application/json;charset=utf-8");
+        exchange.sendResponseHeaders(statusCode, resp.length);
+        OutputStream os = exchange.getResponseBody();
+        os.write(resp);
+        os.close();
+    }
+
+    protected void sendNotFound(HttpExchange exchange) throws IOException {
+        sendText(exchange, "Объект не был найден", 404);
+    }
+
+    protected void sendHasInteractions(HttpExchange exchange) throws IOException {
+        sendText(exchange, "Задача пересекается с уже существующими", 406);
+    }
+}
